d8ec7e1efaf949d0cebd671ea777ca1c78f4bd46
mentrixmax <101875553+mateusvieiraifce@users.noreply.github.com>
2025-10-02 15:06:29 -0300

    subindo aula 5

---
+++ Aula04/main.py
@@ -0,0 +1,69 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+from models import User, Base, engine, Cliente, Venda
+from sqlalchemy.orm import sessionmaker
+
+app = Flask(__name__)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+def consulta_banco(username, password):
+    user =  session.query(User).filter_by(username=username, password=password).first()
+    if not user:
+        return False
+    return True
+
+@app.route('/home')
+def home():
+    return "Hello, World!"
+
+@app.route('/cliente/<int:id>', methods=['GET'])
+def cliente_view(id):
+    cliente = session.query(Cliente).filter_by(id=id).first()
+    if not cliente:
+        return "Cliente not found", 404
+    return cliente.to_dict(), 200
+
+@app.route('/venda/<int:id>', methods=['GET'])
+def venda_by_id(id):
+    venda = session.query(Venda).filter_by(id=id).first()
+    if not venda:
+        return "Venda not found", 404
+    return venda.to_dict(), 200
+
+
+
+@app.route('/user/create')
+def create_user():
+    return render_template('create_user.html')
+
+@app.route('/user/create', methods=['POST'])
+def create_user_do():
+    username = request.form['username']
+    password = request.form['password']
+    new_user = User(username=username, password=password)
+    session.add(new_user)
+    session.commit()
+    return redirect(url_for('form'))
+
+@app.route('/')
+def form():
+    return render_template('form.html')
+
+@app.route('/user/save', methods=['POST'])
+def saveForm():
+    user = request.form['username']
+    password = request.form['password']
+    msgReponse = ""
+    if consulta_banco( user, password ):
+        msgReponse = "Login Successful"
+    else:
+        msgReponse = "Login Failed"
+    
+    return render_template('form.html', msg=msgReponse  )
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
+    app.run(debug=True, port=5001)
